7f3a2832c86346abc0d2a0720fddd6f2340f54e0
Deena Athithyaa D <deenaathithyaa.d2022@vitstudent.ac.in>
2024-02-23 22:32:36 +0530

    added css

--- Employee-Front/script.js
+++ Employee-Front/script.js
@@ -18,3 +18,8 @@ function hideSideBar(event) {
 
   event.preventDefault();
 }
+var pos = document.documentElement;
+pos.addEventListener("mousemove",e=>{
+  pos.style.setProperty("--x",e.clientX+"px")
+  pos.style.setProperty("--y",e.clientY+"px")
+})
