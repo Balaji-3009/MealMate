f153af5df50fc5c48bb993657ceb5cf7a50bc4c9
Deena Athithyaa D <deenaathithyaa.d2022@vitstudent.ac.in>
2024-02-23 21:17:56 +0530

    sidebar complete

--- Employee-Front/script.js
+++ Employee-Front/script.js
@@ -1,10 +1,20 @@
 function showSideBar(event) {
   const sidebar = document.querySelector(".sidebar");
+
   sidebar.style.display = "flex";
+  setTimeout(() => {
+    sidebar.style.left = "0px";
+  }, 0.1);
+
   event.preventDefault();
 }
 function hideSideBar(event) {
   const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "none";
+  sidebar.style.left = "-500px";
+  console.log("Function called");
+  setTimeout(() => {
+    sidebar.style.display = "none";
+  }, 1000);
+
   event.preventDefault();
 }
