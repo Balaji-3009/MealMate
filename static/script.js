d2fc8edd455a0b1b077e87929dbdf916d2f2f36c
Deena Athithyaa D <deenaathithyaa.d2022@vitstudent.ac.in>
2024-02-25 10:00:23 +0530

    animation added

--- static/script.js
+++ static/script.js
@@ -1,25 +1,32 @@
 function showSideBar(event) {
-    const sidebar = document.querySelector(".sidebar");
-  
+  const sidebar = document.querySelector(".sidebar");
+  const mainContent = document.querySelector("#main-content");
+  if (sidebar.style.display === "flex") {
+    hideSideBar(event);
+  } else {
     sidebar.style.display = "flex";
     setTimeout(() => {
       sidebar.style.left = "0px";
     }, 0.1);
-  
-    event.preventDefault();
+    mainContent.style.marginLeft = "350px";
   }
-  function hideSideBar(event) {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.style.left = "-500px";
-    console.log("Function called");
-    setTimeout(() => {
-      sidebar.style.display = "none";
-    }, 1000);
-  
-    event.preventDefault();
-  }
-  // var pos = document.documentElement;
-  // pos.addEventListener("mousemove",e=>{
-  //   pos.style.setProperty("--x",e.clientX+"px")
-  //   pos.style.setProperty("--y",e.clientY+"px")
-  // })
+  event.preventDefault();
+}
+function hideSideBar(event) {
+  const sidebar = document.querySelector(".sidebar");
+  const mainContent = document.querySelector("#main-content");
+  sidebar.style.left = "-400px";
+  console.log("Function called");
+  setTimeout(() => {
+    sidebar.style.display = "none";
+  }, 500);
+  setTimeout(() => {
+    mainContent.style.marginLeft = "200px";
+  }, 20);
+  event.preventDefault();
+}
+// var pos = document.documentElement;
+// pos.addEventListener("mousemove",e=>{
+//   pos.style.setProperty("--x",e.clientX+"px")
+//   pos.style.setProperty("--y",e.clientY+"px")
+// })
