3e2d03ce5a1ae1e30138314a948d5489884f3801
Deena Athithyaa D <deenaathithyaa.d2022@vitstudent.ac.in>
2024-03-08 21:52:49 +0530

    nav bar done

---
+++ static/scriptBase.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var navItems = document.querySelectorAll(".nav-li");
+
+//   navItems.forEach(function (item) {
+//     item.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       navItems.forEach(function (navItem) {
+//         navItem.classList.remove("liActive");
+//       });
+//       item.classList.add("liActive");
+//       var href = item.getAttribute("href");
+//       if (href) {
+//         window.location.href = href;
+//       }
+//     });
+//   });
+//   console.log("Function called");
+// });
