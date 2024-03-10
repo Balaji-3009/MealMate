081193eca27775393a66cfea4d463589d7e44a2b
Deena Athithyaa D <deenaathithyaa.d2022@vitstudent.ac.in>
2024-03-10 21:46:19 +0530

    Pending orders custoemr page done

---
+++ static/pendingOrdersScript.js
@@ -0,0 +1,27 @@
+function showSideBar(event) {
+  const sidebar = document.querySelector(".sidebar");
+  // const mainContent = document.querySelector("#main-content");
+  if (sidebar.style.display === "flex") {
+    hideSideBar(event);
+  } else {
+    sidebar.style.display = "flex";
+    setTimeout(() => {
+      sidebar.style.left = "0px";
+    }, 0.1);
+    // mainContent.style.marginLeft = "350px";
+  }
+  event.preventDefault();
+}
+function hideSideBar(event) {
+  const sidebar = document.querySelector(".sidebar");
+  // const mainContent = document.querySelector("#main-content");
+  sidebar.style.left = "-400px";
+  console.log("Function called");
+  setTimeout(() => {
+    sidebar.style.display = "none";
+  }, 500);
+  setTimeout(() => {
+    // mainContent.style.marginLeft = "200px";
+  }, 20);
+  event.preventDefault();
+}
