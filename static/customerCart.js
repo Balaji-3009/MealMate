799d7264ab58af1aa4de0db2959304f6ca9776cd
Deena Athithyaa D <deenaathithyaa.d2022@vitstudent.ac.in>
2024-04-21 22:33:29 +0530

    css update

--- static/customerCart.js
+++ static/customerCart.js
@@ -1,3 +1,18 @@
+document.querySelector(".studentCart").addEventListener("click", (event) => {
+  const sidebar = document.querySelector(".sidebar");
+  const isButton = event.target.classList.contains("show_button");
+  // Check if the clicked element is not within the sidebar
+  if (!sidebar.contains(event.target) && !isButton) {
+    sidebar.style.left = "-400px";
+    console.log("Function called");
+    setTimeout(() => {
+      sidebar.style.display = "none";
+    }, 500);
+    // setTimeout(() => {
+    //   mainContent.style.marginLeft = "400px";
+    // }, 20);
+  }
+});
 function showSideBar(event) {
   const sidebar = document.querySelector(".sidebar");
   const body = document.querySelector("body");
